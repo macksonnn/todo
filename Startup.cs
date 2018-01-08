b1707a9074dece67cb321f8d4e2bc586ad25642f
Mackson de Castro de Oliveira <mackson.oliveira@fcamara.com.br>
2018-01-08 16:33:18 -0200

    Primeiro commit

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // DbContextOptions<TodoContext> options;
+            // var builder = new DbContextOptionsBuilder<TodoContext>();
+            // builder.UseInMemoryDatabase("TodoList");
+            // options = builder.Options;
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
